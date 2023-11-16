efc2c403822ab4344342ca76f5811c056d5d90d5
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-11-16 17:15:47 +0400

    no end
    no end

--- lesson-4/src/App.jsx
+++ lesson-4/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 
 const App = ()=> {
-  const [name, setName] = useState("")
-  const [lastName, setLastName] = useState("")
+//   const [name, setName] = useState("")
+//   const [lastName, setLastName] = useState("")
 
-  function setNamesLast (value,changed) {
-   if (changed === "name"){
-    setName(value)
-   } else if (changed === "lastName"){
-    setLastName (value)
-   }
-  }
+//   function setNamesLast (value,changed) {
+//    if (changed === "name"){
+//     setName(value)
+//    } else if (changed === "lastName"){
+//     setLastName (value)
+//    }
+//   }
 
- function Clear()  {
-  setName("")
-  setLastName("")
- }
+//  function Clear()  {
+//   setName("")
+//   setLastName("")
+//  }
 
 //  function BottonDisable(){
 //   if (name?.length === 0){
@@ -27,20 +27,76 @@ const App = ()=> {
 //   }
 //  }
 
-  return (
-    <div>
-    name:  {name}
-    <br/>
-    lastName:  {lastName}
-      <br/>
-      <input value={name} type="text" onChange={(event)=> setNamesLast(event.target.value, "name")} />
-      <br/>
-      <input value={lastName} type="text" onChange={(event)=> setNamesLast(event.target.value, "lastName") } />
-      <br/>
-      <button disabled={name.length === 0 && lastName.length === 0 ? true : false} onClick={()=> Clear()} >click me</button>
-    </div>
-  )
+//   return (
+//     <div>
+//     name:  {name}
+//     <br/>
+//     lastName:  {lastName}
+//       <br/>
+//       <input value={name} type="text" onChange={(event)=> setNamesLast(event.target.value, "name")} />
+//       <br/>
+//       <input value={lastName} type="text" onChange={(event)=> setNamesLast(event.target.value, "lastName") } />
+//       <br/>
+//       <button disabled={name.length === 0 && lastName.length === 0 ? true : false} onClick={()=> Clear()} >click me</button>
+//     </div>
+//   )
+
+// const [count, setCount] = useState(0)
+// const [count2, setCount2] = useState(0)
+
+// const [data, setData] = useState([])
+// const [number, setNumber] = useState("10")
+
+// async function FetchData () {
+//     const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${number}`)
+//      const list = await response.json();
+//      setData(list);
+//     //  console.log(list)
+//       // .then(json => console.log(json))
+// }
+
+// useEffect (()=> {
+//  FetchData()
+// },[number])
+
+// return (
+//   <div>
+//     {console.log(data)}
+//     {data.title}
+//     <br/>
+//     <input type="text" onChange={(e)=> setNumber(e.target.value) }/>
+ 
+//    {/* {data.map(data =>(
+//     <p>{data.title}</p>
+//    ))} */}
+
+//     {/* count : {count}
+//     <br/>
+//    <button onClick={()=> setCount(count+1)}>click me</button> */}
+  
+//   </div>
+  
+  
+// )
+
+const [number, setNumber] = useState()
+
+
+
+
+return (
+  <div>
+   {number}
+   <br/>
+  <input type="text" onChange={(event) => setNumber(event.target.value)}/>
+  <button >Search</button>
+  </div>
+  
+)
+
 }
+
+
 
 
 
